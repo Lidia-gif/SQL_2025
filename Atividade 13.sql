30d564ca7915641256502b631a3d15b10e992de7
Lidia-gif <lidiamaria.202069@gmail.com>
2025-10-22 14:57:21 +0100

    Add files via upload

---
+++ Atividade 13.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE China ;
+USE China;
+
+CREATE TABLE cidades (
+  cidadesID INT PRIMARY KEY,
+Nome_Cidade VARCHAR(100) NOT NULL,
+Codigo_postal INT
+);                                                                             
+SELECT * FROM CIDADES;
