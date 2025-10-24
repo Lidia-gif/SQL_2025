e251709dde8b770cb3e35c7fa4b3384b8b8d80b6
Lidia-gif <lidiamaria.202069@gmail.com>
2025-10-24 14:13:41 +0100

    Add files via upload

---
+++ Atividade16.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE Armazenar_Produtos; 
+
+USE  Armazenar_Produtos;
+
+CREATE TABLE Armazenar_Produtos (
+	cod_prodID INT PRIMARY KEY,
+    valor_unit FLOAT,
+    quantidade INT,
+    desconto FLOAT
+
+);
+
+
+       
+SELECT * FROM Armazenar_Produtos;
